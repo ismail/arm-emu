9b9aae5710150bc3dc1d58e2e507436ec6b12184
İsmail Dönmez <ismail@i10z.com>
2021-03-19 16:49:32 +0100

    Add x86 and x86_64 support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
 use std::env;
 use std::fs::File;
 use std::io;
@@ -6,7 +9,6 @@ use std::io::{Error, ErrorKind};
 use std::process::Command;
 
 static ELF_MAGIC: [u8; 4] = [0x7f, 0x45, 0x4c, 0x46];
-const ARM_MACHINE_TYPE: u16 = 40;
 
 // https://refspecs.linuxfoundation.org/elf/gabi4+/ch4.eheader.html
 // unsigned char   e_ident[16]
@@ -19,29 +21,67 @@ enum ELFClass {
     ELFCLASS64,
 }
 
-fn handle_arm(args: &Vec<String>, elf_class: ELFClass) {
+#[derive(FromPrimitive)]
+enum Machine {
+    X86 = 3,
+    ARM = 40,
+    X86_64 = 62,
+}
+
+struct Executable {
+    class: ELFClass,
+    machine: Machine,
+}
+
+fn run_executable(executable: Executable, args: &Vec<String>) -> Result<(), io::Error> {
     let ld_suffix: &str;
     let lib_suffix: &str;
     let qemu_suffix: &str;
     let sysroot: &str = &env::var("SYSROOT").unwrap_or("".to_string());
 
-    match elf_class {
-        ELFClass::ELFCLASS32 => {
-            ld_suffix = "armhf.so.3";
-            lib_suffix = "";
-            qemu_suffix = "arm"
-        }
-        ELFClass::ELFCLASS64 => {
-            ld_suffix = "aarch64.so.1";
-            qemu_suffix = "aarch64";
-            lib_suffix = "64";
+    match executable.class {
+        ELFClass::ELFCLASS32 => match executable.machine {
+            Machine::ARM => {
+                ld_suffix = "-armhf.so.3";
+                lib_suffix = "";
+                qemu_suffix = "arm";
+            }
+            Machine::X86 => {
+                ld_suffix = ".so.2";
+                lib_suffix = "";
+                qemu_suffix = "i386";
+            }
+            _ => {
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    "Invalid executable specification.",
+                ))
+            }
+        },
+        ELFClass::ELFCLASS64 => match executable.machine {
+            Machine::ARM => {
+                ld_suffix = "-aarch64.so.1";
+                qemu_suffix = "aarch64";
+                lib_suffix = "64";
+            }
+            Machine::X86_64 => {
+                ld_suffix = "-x86_64.so.2";
+                qemu_suffix = "x86_64";
+                lib_suffix = "64";
+            }
+            _ => {
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    "Invalid executable specification.",
+                ))
+            }
         }
     }
 
     if sysroot != "" {
         Command::new(format!("/usr/bin/qemu-{}", qemu_suffix))
             .arg(format!(
-                "{}/lib{}/ld-linux-{}",
+                "{}/lib{}/ld-linux{}",
                 sysroot, lib_suffix, ld_suffix
             ))
             .arg("--library-path")
@@ -59,9 +99,11 @@ fn handle_arm(args: &Vec<String>, elf_class: ELFClass) {
             .status()
             .expect(format!("Unable to run /usr/bin/qemu-{}", qemu_suffix).as_str());
     }
+
+    Ok(())
 }
 
-fn get_elf_class(executable: &str) -> Result<ELFClass, io::Error> {
+fn get_executable(executable: &str) -> Result<Executable, io::Error> {
     let f = File::open(&executable)?;
 
     let mut buffer = [0; HEADER_SIZE as usize];
@@ -76,31 +118,43 @@ fn get_elf_class(executable: &str) -> Result<ELFClass, io::Error> {
         ));
     }
 
-    let machine_type: u16 = buffer[18] as u16 + buffer[19] as u16 * 256;
-    if machine_type != ARM_MACHINE_TYPE {
-        return Err(Error::new(
-            ErrorKind::Other,
-            format!(
-                "{} is not an ARM executable, machine type: {}",
-                executable, machine_type,
-            ),
-        ));
-    }
+    let machine_type_value: u16 = buffer[18] as u16 + buffer[19] as u16 * 256;
+    let machine_type: Machine;
+
+    match FromPrimitive::from_u16(machine_type_value) {
+        Some(Machine::ARM) => machine_type = Machine::ARM,
+        Some(Machine::X86) => machine_type = Machine::X86,
+        Some(Machine::X86_64) => machine_type = Machine::X86_64,
+        None => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!(
+                    "{} is not an ARM, x86 or x86_64 executable, machine type: {}",
+                    executable, machine_type_value,
+                ),
+            ));
+        }
+    };
 
     let elfclass = buffer[4];
 
-    match elfclass {
-        1 => return Ok(ELFClass::ELFCLASS32),
-        2 => return Ok(ELFClass::ELFCLASS64),
-        _ => return Err(Error::new(ErrorKind::Other, "Invalid ELF class.")),
-    }
+    let exec = Executable {
+        class: match elfclass {
+            1 => ELFClass::ELFCLASS32,
+            2 => ELFClass::ELFCLASS64,
+            _ => return Err(Error::new(ErrorKind::Other, "Invalid ELF class.")),
+        },
+        machine: machine_type,
+    };
+
+    Ok(exec)
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let elfclass = get_elf_class(&args[1]).unwrap();
+    let executable = get_executable(&args[1]).unwrap();
 
-    handle_arm(&args, elfclass);
+    run_executable(executable, &args).unwrap();
 
     Ok(())
 }
